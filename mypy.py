bd6fdc8b6e04a57862bc6d64b4116366e27db1b0
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-23 23:59:05 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -1,3 +1,19 @@
-i=0
-for i in range(1, 100):
-   print ("sreedevi")
+import xml.etree.ElementTree as XML
+
+def GenerateXML(fileName)
+    root=xml.Element("Customers")
+    cl=xml.Element("customer")
+    root.append(cl)
+
+    type1=xml.SubElement(c1, "Place")
+    type1.text="UK"
+
+    Amount1=xml.SubElement(c1, "Amount1")
+    Amount1.text = "5000"
+
+    tree=xml.ElementTree(root)
+
+    with open(fileName, "wb") as files:
+       tree.write(files)
+if __name__ == __main__:
+    GenerateXML("Cust.XML") 
