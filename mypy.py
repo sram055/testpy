ea6adf801a2a9f6d2577fe0b9fe2a4c81cb5a5e0
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-25 20:11:04 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -1,8 +1,8 @@
 import xml.etree.ElementTree as xml
 import gitapi
-repo = gitapi.Repo("testpy") #existing folder
+repo = gitapi.Repo("sram055/testpy") #existing folder
 repo.git_init()
-repo.git_add("fole\newfile2") #already created but not added file
+repo.git_add("fold/newfile2") #already created but not added file
 repo.git_commit("Adding newfile2", user="sram055")
 str(repo['HEAD'].desc)
 'Adding newfile2'
