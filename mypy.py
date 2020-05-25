8bfabb73622d8b547157cf0189fbfa68bb649dd8
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-26 00:53:11 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -3,7 +3,10 @@ from git import *
 import git
 
 repo = git.Repo("/home/runner/work/testpy/testpy")
-repo.commits ()
+commits = list(repo.iter_commits('master'))
+print(commits[0])
+print(commits[0].stats.total)
+print(commits[0].stats.files)
 
 def GenerateXML(fileName):
     root=xml.Element("Customers")
