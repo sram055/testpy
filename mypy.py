136d776250e4620c5b40481086bb82183489f19c
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-27 23:39:54 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -1,21 +1,17 @@
-import xml.etree.ElementTree as xml
+from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree etree
 
 
-def GenerateXML(fileName):
-    root=xml.Element("Customers")
-    
-    cl=xml.Element("customer")
-    root.append(cl)
-
-    type1=xml.SubElement(cl,"Place")
-    type1.text="UK"
-
-    Amount1=xml.SubElement(cl,"Amount")
-    Amount1.text = "5000"
-
-    tree=xml.ElementTree(root)
-
-    with open(fileName, "wb") as files:
-       tree.write(files, '\n')
+root=xml.Element("Customers")
+tree=xml.ElementTree(root)
+cl=xml.Element("customer")
+root.append(cl)
+type1=xml.SubElement(cl,"Place")
+type1.text="UK"
+Amount1=xml.SubElement(cl,"Amount")
+Amount1.text = "5000"
+  with open(fileName, "w") as files:
+       tree.write(files)
 if __name__=="__main__":
-    GenerateXML("Cuty.xml") 
+    GenerateXML("Custard.xml") 
