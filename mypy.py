594abe882028dd4b4586fd410c67a95d1f6b606a
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-28 00:08:14 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -3,14 +3,15 @@ from xml.etree.ElementTree import Element
 import xml.etree.ElementTree as etree
 
 def GenerateXML(fileName):
-      root=xml.Element("Customers")
-      tree=xml.ElementTree(root)
-      cl=xml.Element("customer")
+      root=Element("Customers")
+      tree=ElementTree(root)
+      cl=Element("customer")
       root.append(cl)
       type1=xml.SubElement(cl,"Place")
       type1.text="UK"
       Amount1=xml.SubElement(cl,"Amount")
       Amount1.text = "5000"
+      etree.tostring(root)
       with open(fileName, "w") as files:
          tree.write(files)
 if __name__=="__main__":
