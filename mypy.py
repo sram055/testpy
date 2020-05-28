1d5084ec452aec930fde9b64a53873fe9b6996d7
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-28 23:50:05 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -6,21 +6,21 @@ import xml.etree.ElementTree as xml
 
 
 def GenerateXML(fileName):
-    root=xml.Element("Customers")
+    root=xml.Element("Package")
     
-    cl=xml.Element("customer")
+    cl=xml.Element("types")
     root.append(cl)
 
-    type1=xml.SubElement(cl,"Place")
-    type1.text="UK"
+    Amount1=xml.SubElement(cl,"members")
+    Amount1.text = "AccountGeneratorTest"
 
-    Amount1=xml.SubElement(cl,"Amount")
-    Amount1.text = "5000"
+    type1=xml.SubElement(cl,"name")
+    type1.text="ApexClass"
 
     tree=xml.ElementTree(root)
 
     with open(fileName, "wb") as files:
        tree.write(files)
 if __name__=="__main__":
-    GenerateXML("moronktly1.xml") 
+    GenerateXML("hero.xml") 
 	
