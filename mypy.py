22d595d9846c2b3a97b44012d14c4f9ea1e7fab5
sram055 <41504162+sram055@users.noreply.github.com>
2020-05-28 11:33:19 +0530

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -22,5 +22,5 @@ def GenerateXML(fileName):
     with open(fileName, "wb") as files:
        tree.write(files)
 if __name__=="__main__":
-    GenerateXML("moronCukty.xml") 
+    GenerateXML("moronkty.xml") 
 	
